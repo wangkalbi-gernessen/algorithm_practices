ed3d463bae3d6e64c674565cd55ad9f51f3607cd
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-02-12 23:55:50 -0800

    Complete sumOfUnique

---
+++ Array/sumOfUnique.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const sumOfUnique = function(nums) {
+  let sortedArray = nums.sort();
+  let count = 1;
+  let result = [];
+  for (let i = 0; i < sortedArray.length; i++) {
+    if (sortedArray[i] === sortedArray[i+1]) {
+      count += 1;
+    } else {
+      if (count > 1) {
+        count = 1;
+      } else {
+        result.push(sortedArray[i]);
+        count = 1;
+      }
+    }
+  }
+  let answer = result.reduce((a,b) => a+b, 0);
+  return answer;
+};
+
+console.log(sumOfUnique([1,1,1,1]));
